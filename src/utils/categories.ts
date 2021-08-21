0e6861809f69e731d5a2f005054942c17d9cd079
pabloMarcondesGS <pablex.marcone@hotmail.com>
2021-08-21 16:04:49 -0300

    correcoes menores

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -4,8 +4,8 @@ import FunSvg from '../assets/fun.svg';
 import TrainingSvg from '../assets/training.svg';
 
 export const categories = [
-  { id: '1', title: 'Cross-fit', icon: RankedSvg },
-  { id: '2', title: 'Lutas', icon: DuelSvg },
-  { id: '3', title: 'Dança', icon: FunSvg },
+  { id: '1', title: 'Passeadores', icon: RankedSvg },
+  // { id: '2', title: 'Lutas', icon: DuelSvg },
+  { id: '3', title: 'Vets', icon: FunSvg },
   { id: '4', title: 'Outros', icon: TrainingSvg },
 ]
